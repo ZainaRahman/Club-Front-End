b1ba9024857b64d18d3e6789039a9984e409f69c
Zaina Rahman <rahman2207021@stud.kuet.ac.bd>
2026-06-09 22:51:39 +0600

    the return to home button fixed

--- member_dashboard.php
+++ member_dashboard.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['username']) || !isset($_SESSION['email'])) {
-    header('Location: login.php');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+// Redirect to login if not logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
     exit();
 }
 
